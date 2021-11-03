01c117edc4d02e63224506de4e6b6405fc018716
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2021-11-03 16:42:42 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href="https://t.me/Rafeeq_Kunnimon">This Person</a></b> \n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/MovieRosterGroup'>Click here</a>\n○ Channel :  @MovieRosterOfficial \n○ Support Group : @MovieRosterGroup</b>",
+            text = f"<b>○ Creator : <a href="https://t.me/Rafeeq_Kunnimon">This Person</a></b> \n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/MovieRosterGroup'>Click here</a>\n○ Channel :  @MovieRosterOfficial\n○ Support Group : @MovieRosterGroup</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
