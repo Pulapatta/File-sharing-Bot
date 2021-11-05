a6b66faef4b9a533c508e6949a9e1da1b5ef2d93
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2021-11-05 20:05:12 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -24,8 +24,8 @@ async def channel_post(client: Client, message: Message):
     converted_id = post_message.message_id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
     
+
     await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
@@ -40,7 +40,6 @@ async def new_post(client: Client, message: Message):
     converted_id = message.message_id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
